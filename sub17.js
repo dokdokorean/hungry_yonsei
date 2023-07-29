f602c50b3be4592675a758d0445996b16912dac7
BAE JINWOO <92303070+dokdokorean@users.noreply.github.com>
2023-07-30 02:56:38 +0900

    Create sub17.js

---
+++ sub17.js
@@ -0,0 +1,22 @@
+window.onload = function main(){
+    //현재 년도 표시
+let date = new Date();
+var year = date.getFullYear()
+//현재 달 표시
+var month = date.getMonth()
+//현재 날짜 표시
+var day = date.getDate()
+//현재 요일 표시
+var day1 = date.getDay()
+
+let hours=date.getHours();
+
+if(day1<6 && hours>10.5 && hours < 18.5){
+    document.getElementById('closed').style.visibility="hidden"
+    document.getElementById('open').style.visibility='visible'
+}
+else{
+    document.getElementById('closed').style.visibility="visible"
+    document.getElementById('open').style.visibility='hidden'
+}
+}

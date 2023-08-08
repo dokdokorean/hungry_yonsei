91145f1ee9202d8499d341fa60ded914e7efaea0
BAE JINWOO <92303070+dokdokorean@users.noreply.github.com>
2023-08-08 13:00:09 +0900

    Create weekly.js

---
+++ weekly.js
@@ -0,0 +1,124 @@
+//월요일
+
+//중식메뉴 1번
+document.getElementById('china-menu10-1').innerText='월요일 중국메뉴1'
+//중식메뉴 2번
+document.getElementById('china-menu20-1').innerText='중국메뉴2'
+//중식메뉴 3번
+document.getElementById('china-menu30-1').innerText='중국메뉴3'
+//중식메뉴 4번
+document.getElementById('china-menu40-1').innerText='중국메뉴4'
+//중식메뉴 5번
+document.getElementById('china-menu50-1').innerText='중국메뉴5'
+
+
+//점심메뉴 1번 (국이름만 수정)
+document.getElementById('lunch-menu1-1').innerText='백반 - '+ '월요일'
+//점심메뉴 2번 
+document.getElementById('lunch-menu2-1').innerText='소고기'
+//점심메뉴 3번 
+document.getElementById('lunch-menu3-1').innerText='소고기'
+//점심메뉴 4번 
+document.getElementById('lunch-menu4-1').innerText='소고기'
+//점심메뉴 5번 
+document.getElementById('lunch-menu5-1').innerText='소고기'
+
+//화요일
+
+//중식메뉴 1번
+document.getElementById('china-menu10-2').innerText='화요일 중국메뉴1'
+//중식메뉴 2번
+document.getElementById('china-menu20-2').innerText='중국메뉴2'
+//중식메뉴 3번
+document.getElementById('china-menu30-2').innerText='중국메뉴3'
+//중식메뉴 4번
+document.getElementById('china-menu40-2').innerText='중국메뉴4'
+//중식메뉴 5번
+document.getElementById('china-menu50-2').innerText='중국메뉴5'
+
+
+//점심메뉴 1번 (국이름만 수정)
+document.getElementById('lunch-menu1-2').innerText='백반 - '+ '화요일'
+//점심메뉴 2번 
+document.getElementById('lunch-menu2-2').innerText='소고기'
+//점심메뉴 3번 
+document.getElementById('lunch-menu3-2').innerText='소고기'
+//점심메뉴 4번 
+document.getElementById('lunch-menu4-2').innerText='소고기'
+//점심메뉴 5번 
+document.getElementById('lunch-menu5-2').innerText='소고기'
+
+//수요일
+
+//중식메뉴 1번
+document.getElementById('china-menu10-3').innerText='수요일 중국메뉴1'
+//중식메뉴 2번
+document.getElementById('china-menu20-3').innerText='중국메뉴2'
+//중식메뉴 3번
+document.getElementById('china-menu30-3').innerText='중국메뉴3'
+//중식메뉴 4번
+document.getElementById('china-menu40-3').innerText='중국메뉴4'
+//중식메뉴 5번
+document.getElementById('china-menu50-3').innerText='중국메뉴5'
+
+
+//점심메뉴 1번 (국이름만 수정)
+document.getElementById('lunch-menu1-3').innerText='백반 - '+ '수요일'
+//점심메뉴 2번 
+document.getElementById('lunch-menu2-3').innerText='소고기'
+//점심메뉴 3번 
+document.getElementById('lunch-menu3-3').innerText='소고기'
+//점심메뉴 4번 
+document.getElementById('lunch-menu4-3').innerText='소고기'
+//점심메뉴 5번 
+document.getElementById('lunch-menu5-3').innerText='소고기'
+
+//목요일
+
+//중식메뉴 1번
+document.getElementById('china-menu10-4').innerText='목요일 중국메뉴1'
+//중식메뉴 2번
+document.getElementById('china-menu20-4').innerText='중국메뉴2'
+//중식메뉴 3번
+document.getElementById('china-menu30-4').innerText='중국메뉴3'
+//중식메뉴 4번
+document.getElementById('china-menu40-4').innerText='중국메뉴4'
+//중식메뉴 5번
+document.getElementById('china-menu50-4').innerText='중국메뉴5'
+
+
+//점심메뉴 1번 (국이름만 수정)
+document.getElementById('lunch-menu1-4').innerText='백반 - '+ '목요일'
+//점심메뉴 2번 
+document.getElementById('lunch-menu2-4').innerText='소고기'
+//점심메뉴 3번 
+document.getElementById('lunch-menu3-4').innerText='소고기'
+//점심메뉴 4번 
+document.getElementById('lunch-menu4-4').innerText='소고기'
+//점심메뉴 5번 
+document.getElementById('lunch-menu5-4').innerText='소고기'
+
+//금요일
+
+//중식메뉴 1번
+document.getElementById('china-menu10-5').innerText='금요일 중국메뉴1'
+//중식메뉴 2번
+document.getElementById('china-menu20-5').innerText='중국메뉴2'
+//중식메뉴 3번
+document.getElementById('china-menu30-5').innerText='중국메뉴3'
+//중식메뉴 4번
+document.getElementById('china-menu40-5').innerText='중국메뉴4'
+//중식메뉴 5번
+document.getElementById('china-menu50-5').innerText='중국메뉴5'
+
+
+//점심메뉴 1번 (국이름만 수정)
+document.getElementById('lunch-menu1-5').innerText='백반 - '+ '금요일'
+//점심메뉴 2번 
+document.getElementById('lunch-menu2-5').innerText='소고기'
+//점심메뉴 3번 
+document.getElementById('lunch-menu3-5').innerText='소고기'
+//점심메뉴 4번 
+document.getElementById('lunch-menu4-5').innerText='소고기'
+//점심메뉴 5번 
+document.getElementById('lunch-menu5-5').innerText='소고기'

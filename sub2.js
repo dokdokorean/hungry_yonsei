34a4a0efdc4fee25507365709755a3399a990518
ldH <92303070+dokdokorean@users.noreply.github.com>
2023-09-02 18:58:12 +0900

    Update sub2.js

--- sub2.js
+++ sub2.js
@@ -13,7 +13,7 @@ let min = date.getMinutes()/60;
 
 let hours=date.getHours()+min;
 
-if( day1 >0 && day1<6 && hours>=8.5 && hours < 18.5 ){
+if( day1 >0 && day1<6 && hours>=8.5 && hours < 17.5 ){
     document.getElementById('closed').style.visibility="hidden"
     document.getElementById('open').style.visibility='visible'
 }
@@ -25,8 +25,10 @@ else{
 var exit =document.getElementById('exit');
     exit.addEventListener('click',function(event){
         window.close('sub1.html')
+        alert('closeWebView')
     })
 var logo =document.getElementById('logo');
     logo.addEventListener('click',function(event){
         window.close('sub1.html')
+        alert('closeWebView')
     })

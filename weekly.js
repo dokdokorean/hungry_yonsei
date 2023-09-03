c7279e3433da7951f1d78cfda9b5b7c4603119a5
ldH <92303070+dokdokorean@users.noreply.github.com>
2023-09-03 11:57:09 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -1,174 +1,174 @@
 //월요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-1').innerText='test'
+document.getElementById('china-menu10-1').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-1').innerText='오픈전'
+document.getElementById('china-menu20-1').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-1').innerText='오픈전'
+document.getElementById('china-menu30-1').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-1').innerText='오픈전'
+document.getElementById('china-menu40-1').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-1').innerText='오픈전'
+document.getElementById('china-menu50-1').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-1').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-1').innerText='백반 - '+ '배추된장국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-1').innerText='오픈전'
+document.getElementById('lunch-menu2-1').innerText='얼큰소고기콩나물해장국'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-1').innerText='오픈전'
+document.getElementById('lunch-menu3-1').innerText='참치마요덮밥'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-1').innerText='오픈전'
+document.getElementById('lunch-menu4-1').innerText='일식카레'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-1').innerText='오픈전'
+document.getElementById('lunch-menu5-1').innerText=''
 
 //화요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-2').innerText='오픈전'
+document.getElementById('china-menu10-2').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-2').innerText='오픈전'
+document.getElementById('china-menu20-2').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-2').innerText='오픈전'
+document.getElementById('china-menu30-2').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-2').innerText='오픈전'
+document.getElementById('china-menu40-2').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-2').innerText='오픈전'
+document.getElementById('china-menu50-2').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-2').innerText='백반 - '+ '콩나물국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='오픈전'
+document.getElementById('lunch-menu2-2').innerText='돈까스김치나베'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='오픈전'
+document.getElementById('lunch-menu3-2').innerText='소고기미역국'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='오픈전'
+document.getElementById('lunch-menu4-2').innerText='부타동'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText='오픈전'
+document.getElementById('lunch-menu5-2').innerText=''
 
 //수요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-3').innerText='오픈전'
+document.getElementById('china-menu10-3').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-3').innerText='오픈전'
+document.getElementById('china-menu20-3').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-3').innerText='오픈전'
+document.getElementById('china-menu30-3').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-3').innerText='오픈전'
+document.getElementById('china-menu40-3').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-3').innerText='오픈전'
+document.getElementById('china-menu50-3').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-3').innerText='백반 - '+ '유부미소국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-3').innerText='오픈전'
+document.getElementById('lunch-menu2-3').innerText='오므라이스'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='오픈전'
+document.getElementById('lunch-menu3-3').innerText='스팸김치덮밥'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='오픈전'
+document.getElementById('lunch-menu4-3').innerText='황태순두부콩나물국'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText='오픈전'
+document.getElementById('lunch-menu5-3').innerText=''
 
 //목요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-4').innerText='오픈전'
+document.getElementById('china-menu10-4').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-4').innerText='오픈전'
+document.getElementById('china-menu20-4').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-4').innerText='오픈전'
+document.getElementById('china-menu30-4').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-4').innerText='오픈전'
+document.getElementById('china-menu40-4').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-4').innerText='오픈전'
+document.getElementById('china-menu50-4').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-4').innerText='백반 - '+ '미역국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='오픈전'
+document.getElementById('lunch-menu2-4').innerText='제육볶음'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='오픈전'
+document.getElementById('lunch-menu3-4').innerText='제육돈까스'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='오픈전'
+document.getElementById('lunch-menu4-4').innerText='소고기무국'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText='오픈전'
+document.getElementById('lunch-menu5-4').innerText=''
 
 //금요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-5').innerText='오픈전'
+document.getElementById('china-menu10-5').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-5').innerText='오픈전'
+document.getElementById('china-menu20-5').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-5').innerText='오픈전'
+document.getElementById('china-menu30-5').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-5').innerText='오픈전'
+document.getElementById('china-menu40-5').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-5').innerText='오픈전'
+document.getElementById('china-menu50-5').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-5').innerText='백반 - '+ '달걀국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='오픈전'
+document.getElementById('lunch-menu2-5').innerText='설렁탕'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='오픈전'
+document.getElementById('lunch-menu3-5').innerText='닭갈비'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText='오픈전'
+document.getElementById('lunch-menu4-5').innerText='새우볶음밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-5').innerText='오픈전'
+document.getElementById('lunch-menu5-5').innerText=''
 
 //토요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-6').innerText='오픈전'
+document.getElementById('china-menu10-6').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-6').innerText='오픈전'
+document.getElementById('china-menu20-6').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-6').innerText='오픈전'
+document.getElementById('china-menu30-6').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-6').innerText='오픈전'
+document.getElementById('china-menu40-6').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-6').innerText='오픈전'
+document.getElementById('china-menu50-6').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-6').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-6').innerText='백반 - '+ ''
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-6').innerText='오픈전'
+document.getElementById('lunch-menu2-6').innerText=''
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-6').innerText='오픈전'
+document.getElementById('lunch-menu3-6').innerText=''
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-6').innerText='오픈전'
+document.getElementById('lunch-menu4-6').innerText=''
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-6').innerText='오픈전'
+document.getElementById('lunch-menu5-6').innerText=''
 
 //일요일
 
 //중식메뉴 1번
-document.getElementById('china-menu10-7').innerText='오픈전'
+document.getElementById('china-menu10-7').innerText=''
 //중식메뉴 2번
-document.getElementById('china-menu20-7').innerText='오픈전'
+document.getElementById('china-menu20-7').innerText=''
 //중식메뉴 3번
-document.getElementById('china-menu30-7').innerText='오픈전'
+document.getElementById('china-menu30-7').innerText=''
 //중식메뉴 4번
-document.getElementById('china-menu40-7').innerText='오픈전'
+document.getElementById('china-menu40-7').innerText=''
 //중식메뉴 5번
-document.getElementById('china-menu50-7').innerText='오픈전'
+document.getElementById('china-menu50-7').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-7').innerText='백반 - '+ '오픈전'
+document.getElementById('lunch-menu1-7').innerText='백반 - '+ ''
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-7').innerText='오픈전'
+document.getElementById('lunch-menu2-7').innerText=''
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-7').innerText='오픈전'
+document.getElementById('lunch-menu3-7').innerText=''
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-7').innerText='오픈전'
+document.getElementById('lunch-menu4-7').innerText=''
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-7').innerText='오픈전'
+document.getElementById('lunch-menu5-7').innerText=''

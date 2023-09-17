f2a848dbd94b2261ece2632605c59da3d242955f
ldH <92303070+dokdokorean@users.noreply.github.com>
2023-09-17 14:37:31 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -1,95 +1,82 @@
 //월요일
 
+//중식특식
+document.getElementById('china-special2-1').innerText='마파두부덮밥'
+
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-1').innerText='백반 - '+ '어묵국'
+document.getElementById('lunch-menu1-1').innerText='백반 - '+ '달걀국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-1').innerText='참치김치찌개'
+document.getElementById('lunch-menu2-1').innerText='김치순두부찌개'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-1').innerText='버터장조림덮밥'
+document.getElementById('lunch-menu3-1').innerText='된장찌개&불고기 SET'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-1').innerText='부대찌개'
+document.getElementById('lunch-menu4-1').innerText='떡갈비마요덮밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-1').innerText='떡볶이&순대'
+document.getElementById('lunch-menu5-1').innerText='나시고랭&점보춘권 SET'
 
 //화요일
 
+//중식특식
+document.getElementById('china-special2-2').innerText='불짜장면'
+
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='백반 - '+ '콩나물국'
+document.getElementById('lunch-menu1-2').innerText='백반 - '+ '미역국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='얼큰소고기무국'
+document.getElementById('lunch-menu2-2').innerText='얼큰소고기콩나물해장국'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='가츠동'
+document.getElementById('lunch-menu3-2').innerText='닭갈비덮밥'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='닭가슴살 하이라이스'
+document.getElementById('lunch-menu4-2').innerText='잔치국수'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText='가쓰오우동'
+document.getElementById('lunch-menu5-2').innerText='일식카레 & 소시지 or 새우튀김'
 
 //수요일
 
+//중식특식
+document.getElementById('china-special2-3').innerText='중화오징어덮밥'
+
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='백반 - '+ '얼갈이된장국'
+document.getElementById('lunch-menu1-3').innerText='백반 - '+ '유뷰미소국'
 //점심메뉴 2번 
 document.getElementById('lunch-menu2-3').innerText='소고기미역국'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='소불고기'
+document.getElementById('lunch-menu3-3').innerText='명란버터밥 & 후라이'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='치킨마요덮밥'
+document.getElementById('lunch-menu4-3').innerText='참치김치덮밥 & 후라이'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText='미트볼덮밥'
+document.getElementById('lunch-menu5-3').innerText='나가사키짬뽕'
 
 //목요일
 
+//중식특식
+document.getElementById('china-special2-4').innerText='류산슬덮밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='백반 - '+ '유부미소국'
+document.getElementById('lunch-menu1-4').innerText='백반 - '+ '콩나물국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='햄김치볶음밥'
+document.getElementById('lunch-menu2-4').innerText='돼지고기김치찌개'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='닭곰탕'
+document.getElementById('lunch-menu3-4').innerText='소고기무국'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='제육볶음'
+document.getElementById('lunch-menu4-4').innerText='차슈덮밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText='돼지고기 김치찌개'
+document.getElementById('lunch-menu5-4').innerText='닭가슴살데리야끼볶음밥'
 
 //금요일
 
+//중식특식
+document.getElementById('china-special2-5').innerText='미운영'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='백반 - '+ '달걀국'
+document.getElementById('lunch-menu1-5').innerText='백반 - '+ '배추된장국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='순대국밥'
+document.getElementById('lunch-menu2-5').innerText='부대찌개'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='햄야채볶음밥&미니핫도그'
+document.getElementById('lunch-menu3-5').innerText='설렁탕'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText='닭갈비'
+document.getElementById('lunch-menu4-5').innerText='잡채볶음밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-5').innerText='사골우거지 해장국'
-
-//토요일
+document.getElementById('lunch-menu5-5').innerText=''
 
 
-//점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-6').innerText='백반 - '+ ''
-//점심메뉴 2번 
-document.getElementById('lunch-menu2-6').innerText=''
-//점심메뉴 3번 
-document.getElementById('lunch-menu3-6').innerText=''
-//점심메뉴 4번 
-document.getElementById('lunch-menu4-6').innerText=''
-//점심메뉴 5번 
-document.getElementById('lunch-menu5-6').innerText=''
-
-//일요일
-
-
-//점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-7').innerText='백반 - '+ ''
-//점심메뉴 2번 
-document.getElementById('lunch-menu2-7').innerText=''
-//점심메뉴 3번 
-document.getElementById('lunch-menu3-7').innerText=''
-//점심메뉴 4번 
-document.getElementById('lunch-menu4-7').innerText=''
-//점심메뉴 5번 
-document.getElementById('lunch-menu5-7').innerText=''

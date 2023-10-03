c2294b27fd800b09e02db34da2931b9dfbf2c2c6
ldH <92303070+dokdokorean@users.noreply.github.com>
2023-10-03 03:26:16 +0000

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -1,82 +1,82 @@
 //월요일
 
 //중식특식
-document.getElementById('china-special2-1').innerText='마파두부덮밥'
+document.getElementById('china-special2-1').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-1').innerText='천원의 아침밥 - '+ '미트볼조림,연두부&양념장'
+document.getElementById('lunch-menu1-1').innerText='천원의 아침밥 - '+ '금주는'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-1').innerText='소고기무국 - 6000원'
+document.getElementById('lunch-menu2-1').innerText='추석연휴로 인해'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-1').innerText='부대찌개 - 6000원'
+document.getElementById('lunch-menu3-1').innerText='메뉴가'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-1').innerText='오므라이스 - 6500원'
+document.getElementById('lunch-menu4-1').innerText='제공되지'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-1').innerText='마라제육볶음 - 7000원'
+document.getElementById('lunch-menu5-1').innerText='않습니다'
 
 //화요일
 
 //중식특식
-document.getElementById('china-special2-2').innerText='불짜장면'
+document.getElementById('china-special2-2').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - '+ '런천미트,도시락김'
+document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - '+ '금주는'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='순두부찌개 - 6000원'
+document.getElementById('lunch-menu2-2').innerText='추석 연휴로 인해'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='참치김치볶음밥 - 6000원'
+document.getElementById('lunch-menu3-2').innerText='메뉴가'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='돼지국밥 - 7000원'
+document.getElementById('lunch-menu4-2').innerText='제공되지'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText='버터장조림덮밥 - 6500원'
+document.getElementById('lunch-menu5-2').innerText='않습니다'
 
 //수요일
 
 //중식특식
-document.getElementById('china-special2-3').innerText='중화오징어덮밥'
+document.getElementById('china-special2-3').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - '+ '사라다빵,소시지,시리얼&우유'
+document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - '+ '금주는'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-3').innerText='소고기미역국 - 6000원'
+document.getElementById('lunch-menu2-3').innerText='추석연휴로 인해'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='육개장 - 7000원'
+document.getElementById('lunch-menu3-3').innerText='메뉴가'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='돈까스 - 7000원'
+document.getElementById('lunch-menu4-3').innerText='제공되지'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText='소불고기볶음밥 - 6200원'
+document.getElementById('lunch-menu5-3').innerText='않습니다'
 
 //목요일
 
 //중식특식
-document.getElementById('china-special2-4').innerText='미운영'
+document.getElementById('china-special2-4').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='백반 - '+ '추석연휴'
+document.getElementById('lunch-menu1-4').innerText='백반 - '+ '금주는'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='추석연휴'
+document.getElementById('lunch-menu2-4').innerText='추석연휴로 인해'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='추석연휴'
+document.getElementById('lunch-menu3-4').innerText='메뉴가'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='추석연휴'
+document.getElementById('lunch-menu4-4').innerText='제공되지'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText='추석연휴'
+document.getElementById('lunch-menu5-4').innerText='않습니다'
 
 //금요일
 
 //중식특식
-document.getElementById('china-special2-5').innerText='미운영'
+document.getElementById('china-special2-5').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='백반 - '+ '추석연휴'
+document.getElementById('lunch-menu1-5').innerText='백반 - '+ '금주는'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='추석연휴'
+document.getElementById('lunch-menu2-5').innerText='추석연휴로 인해'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='추석연휴'
+document.getElementById('lunch-menu3-5').innerText='메뉴가'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText='추석연휴'
+document.getElementById('lunch-menu4-5').innerText='제공되지'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-5').innerText='추석연휴'
+document.getElementById('lunch-menu5-5').innerText='않습니다'
 
 

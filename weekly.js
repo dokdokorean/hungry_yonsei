89bbd296a3a29a9d9a720b67f42603cad81bc590
baeyousung <142093077+baeyousung@users.noreply.github.com>
2023-10-08 02:32:13 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -20,15 +20,15 @@ document.getElementById('lunch-menu5-1').innerText='한글날'
 document.getElementById('china-special2-2').innerText='불짜장면'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='콩나물황태순두부국'
+document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - 제육 + 요구르트'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='고추참치새싹비빔밥'
+document.getElementById('lunch-menu2-2').innerText='콩나물황태순두부국 : 6,000원'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='얼큰소고기무국'
+document.getElementById('lunch-menu3-2').innerText='고추참치새싹비빔밥 : 6,500원'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='일식카레&가라아게'
+document.getElementById('lunch-menu4-2').innerText='얼큰소고기무국 : 6,000원'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText=''
+document.getElementById('lunch-menu5-2').innerText='일식카레&가라아게 : 6,800원'
 
 //수요일
 
@@ -37,15 +37,15 @@ document.getElementById('china-special2-3').innerText='중화오징어덮밥'
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='부대찌개'
+document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - 비엔나야채볶음 + 도시락김'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-3').innerText='돼지김치두루치기'
+document.getElementById('lunch-menu2-3').innerText='부대찌개 : 6,000원'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='닭가슴살칼국수'
+document.getElementById('lunch-menu3-3').innerText='돼지김치두루치기 : 6,500원'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='베이컨갈릭볶음밥&미니핫도그'
+document.getElementById('lunch-menu4-3').innerText='닭칼국수 :6,500원'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText=''
+document.getElementById('lunch-menu5-3').innerText='베이컨갈릭볶음밥&미니핫도그 : 6,800원'
 
 //목요일
 
@@ -53,15 +53,15 @@ document.getElementById('lunch-menu5-3').innerText=''
 document.getElementById('china-special2-4').innerText='고추덮밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='시골들깨미역국'
+document.getElementById('lunch-menu1-4').innerText='천원의 아침밥 - 런천미트 + 연두부, 양념장'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='치킨마요덮밥'
+document.getElementById('lunch-menu2-4').innerText='사골들깨미역국 : 5,800원'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='돼지고기김치찌개'
+document.getElementById('lunch-menu3-4').innerText='치킨마요덮밥 : 6,000원'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='불고기볶음밥'
+document.getElementById('lunch-menu4-4').innerText='돼지고기김치찌개 : 6,000원'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText=''
+document.getElementById('lunch-menu5-4').innerText='불고기볶음밥'
 
 //금요일
 
@@ -69,11 +69,11 @@ document.getElementById('lunch-menu5-4').innerText=''
 document.getElementById('china-special2-5').innerText='양식 DAY'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='돈까스'
+document.getElementById('lunch-menu1-5').innerText=''
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='파스타'
+document.getElementById('lunch-menu2-5').innerText='돈까스'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText=''
+document.getElementById('lunch-menu3-5').innerText='파스타'
 //점심메뉴 4번 
 document.getElementById('lunch-menu4-5').innerText=''
 //점심메뉴 5번 

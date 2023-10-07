2de23a361de3a6ec4e0c19ab93b653d88dd72b26
baeyousung <142093077+baeyousung@users.noreply.github.com>
2023-10-08 02:24:20 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -17,66 +17,66 @@ document.getElementById('lunch-menu5-1').innerText='한글날'
 //화요일
 
 //중식특식
-document.getElementById('china-special2-2').innerText=''
+document.getElementById('china-special2-2').innerText='불짜장면'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - '+ '금주는'
+document.getElementById('lunch-menu1-2').innerText='콩나물황태순두부국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='추석 연휴로 인해'
+document.getElementById('lunch-menu2-2').innerText='고추참치새싹비빔밥'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='메뉴가'
+document.getElementById('lunch-menu3-2').innerText='얼큰소고기무국'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='제공되지'
+document.getElementById('lunch-menu4-2').innerText='일식카레&가라아게'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText='않습니다'
+document.getElementById('lunch-menu5-2').innerText=''
 
 //수요일
 
 //중식특식
-document.getElementById('china-special2-3').innerText=''
+document.getElementById('china-special2-3').innerText='중화오징어덮밥'
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - '+ '금주는'
+document.getElementById('lunch-menu1-3').innerText='부대찌개'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-3').innerText='추석 연휴로 인해'
+document.getElementById('lunch-menu2-3').innerText='돼지김치두루치기'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='메뉴가'
+document.getElementById('lunch-menu3-3').innerText='닭가슴살칼국수'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='제공되지'
+document.getElementById('lunch-menu4-3').innerText='베이컨갈릭볶음밥&미니핫도그'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText='않습니다'
+document.getElementById('lunch-menu5-3').innerText=''
 
 //목요일
 
 //중식특식
-document.getElementById('china-special2-4').innerText=''
+document.getElementById('china-special2-4').innerText='고추덮밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='백반 - '+ '금주는'
+document.getElementById('lunch-menu1-4').innerText='시골들깨미역국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='추석 연휴로 인해'
+document.getElementById('lunch-menu2-4').innerText='치킨마요덮밥'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='메뉴가'
+document.getElementById('lunch-menu3-4').innerText='돼지고기김치찌개'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='제공되지'
+document.getElementById('lunch-menu4-4').innerText='불고기볶음밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText='않습니다'
+document.getElementById('lunch-menu5-4').innerText=''
 
 //금요일
 
 //중식특식
-document.getElementById('china-special2-5').innerText=''
+document.getElementById('china-special2-5').innerText='양식 DAY'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='백반 - '+ '금주는'
+document.getElementById('lunch-menu1-5').innerText='돈까스'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='추석 연휴로 인해'
+document.getElementById('lunch-menu2-5').innerText='파스타'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='메뉴가'
+document.getElementById('lunch-menu3-5').innerText=''
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText='제공되지'
+document.getElementById('lunch-menu4-5').innerText=''
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-5').innerText='않습니다'
+document.getElementById('lunch-menu5-5').innerText=''
 
 

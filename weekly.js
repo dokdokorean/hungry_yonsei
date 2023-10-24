7838b20ad5bc2fa17ff0e102917770ef9504c325
baeyousung <142093077+baeyousung@users.noreply.github.com>
2023-10-25 04:18:38 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -1,82 +1,82 @@
 //월요일
 
 //중식특식
-document.getElementById('china-special2-1').innerText='마파두부덮밥'
+document.getElementById('china-special2-1').innerText='짜장밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-1').innerText='천원의 아침밥 - 로제소스미트볼 + 셀러드'
+document.getElementById('lunch-menu1-1').innerText='천원의 아침밥 - 너비아니 & 데리야끼 + 연두부 & 양념장 + 얼큰 콩나물국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-1').innerText='김치볶음밥&소시지'
+document.getElementById('lunch-menu2-1').innerText='잔치국수'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-1').innerText='소고기무국'
+document.getElementById('lunch-menu3-1').innerText='사골들깨미역국'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-1').innerText='부대찌개'
+document.getElementById('lunch-menu4-1').innerText='짬뽕밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-1').innerText='닭고기카레덮밥'
+document.getElementById('lunch-menu5-1').innerText='두부찌개'
 
 //화요일
 
 //중식특식
-document.getElementById('china-special2-2').innerText='불짜장면'
+document.getElementById('china-special2-2').innerText='마파두부덮밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - 가라아게강정 + 김자반'
+document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - 미니돈가스 + 김자반 + 계란국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='돼지국밥'
+document.getElementById('lunch-menu2-2').innerText='사골해물칼국수'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='해물순두부찌게'
+document.getElementById('lunch-menu3-2').innerText='닭곰탕'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='참치마요덮밥'
+document.getElementById('lunch-menu4-2').innerText='얼큰소고기무국'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText='규동'
+document.getElementById('lunch-menu5-2').innerText='오삼볶음'
 
 //수요일
 
 //중식특식
-document.getElementById('china-special2-3').innerText='중화오징어덮밥'
+document.getElementById('china-special2-3').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - 참치김치찌개 + 연두부'
+document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - 칼집비엔나버섯볶음 + 샐러드&참깨드레싱 + 유부된장국'
 //점심메뉴 2번 
 document.getElementById('lunch-menu2-3').innerText='사골우거지해장국'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='돼지볶음고추장비빔밥'
+document.getElementById('lunch-menu3-3').innerText='순대국밥'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='모둠까스'
+document.getElementById('lunch-menu4-3').innerText='소고기미역국'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText='돼지고기김치찌개'
+document.getElementById('lunch-menu5-3').innerText='김치볶음밥(후라이)&크리미어니언치킨'
 
 //목요일
 
 //중식특식
-document.getElementById('china-special2-4').innerText='고추덮밥'
+document.getElementById('china-special2-4').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='천원의 아침밥 - 비엔나조림 + 참치콘샐러드'
+document.getElementById('lunch-menu1-4').innerText='천원의 아침밥 -모듬장조림 + 상추겉절이'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='사골들깨미역국'
+document.getElementById('lunch-menu2-4').innerText='된장찌개&불고기'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='지코바st닭고기덮밥'
+document.getElementById('lunch-menu3-4').innerText='육개장'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='육개장'
+document.getElementById('lunch-menu4-4').innerText='부대찌개'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText='고추덮밥'
+document.getElementById('lunch-menu5-4').innerText='베이컨카레볶음밥(후라이)&소시지'
 
 //금요일
 
 //중식특식
-document.getElementById('china-special2-5').innerText='메뉴 미정'
+document.getElementById('china-special2-5').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='천원의 아침밥 - 치킨너겟 + 도시락김'
+document.getElementById('lunch-menu1-5').innerText='천원의 아침밥 - 떡갈비조림 + 나물무침 + 배추장국'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu2-5').innerText='돼지짜글이찌개'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu3-5').innerText='돈까스'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu4-5').innerText='냄비우동&볶음김치주먹밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu5-5').innerText=''
 
 

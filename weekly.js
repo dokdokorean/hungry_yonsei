ba9036387b35ff566c96b97f3da641ea5f97023a
ldH <92303070+dokdokorean@users.noreply.github.com>
2023-11-05 10:13:49 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -4,13 +4,13 @@
 document.getElementById('china-special2-1').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-1').innerText='천원의 아침밥 - 함박스테이크 + 해쉬브라운&케찹 + 양송이스프'
+document.getElementById('lunch-menu1-1').innerText='학식모지 업데이트 일정으로 인해'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-1').innerText='소고기미역국'
+document.getElementById('lunch-menu2-1').innerText='화요일부터 서비스를 재개할 예정입니다!'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-1').innerText='매콤소불고기덮밥'
+document.getElementById('lunch-menu3-1').innerText='많은 기대 부탁드립니다!'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-1').innerText='소보로비빔밥(돈육)'
+document.getElementById('lunch-menu4-1').innerText=''
 //점심메뉴 5번 
 document.getElementById('lunch-menu5-1').innerText=''
 
@@ -20,13 +20,13 @@ document.getElementById('lunch-menu5-1').innerText=''
 document.getElementById('china-special2-2').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='천원의 아침밥 - 두부조림 + 참치콘샐러드 + 얼갈이된장국'
+document.getElementById('lunch-menu1-2').innerText='학식모지 업데이트 일정으로 인해'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='돼지김치찌개'
+document.getElementById('lunch-menu2-2').innerText='화요일부터 서비스를 재개할 예정입니다!'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='어묵채우동'
+document.getElementById('lunch-menu3-2').innerText='많은 기대 부탁드립니다!'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText='햄야채볶음밥&닭강정 set'
+document.getElementById('lunch-menu4-2').innerText=''
 //점심메뉴 5번 
 document.getElementById('lunch-menu5-2').innerText=''
 
@@ -37,13 +37,13 @@ document.getElementById('china-special2-3').innerText=''
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='천원의 아침밥 - 순대떡볶음 + 청포묵김가루무침 + 팽이부추계란국'
+document.getElementById('lunch-menu1-3').innerText='학식모지 업데이트 일정으로 인해'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-3').innerText='사골우거지해장국'
+document.getElementById('lunch-menu2-3').innerText='화요일부터 서비스를 재개할 예정입니다!'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='치킨마요덮밥'
+document.getElementById('lunch-menu3-3').innerText='많은 기대 부탁드립니다!'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText='돼지국밥'
+document.getElementById('lunch-menu4-3').innerText=''
 //점심메뉴 5번 
 document.getElementById('lunch-menu5-3').innerText=''
 
@@ -53,13 +53,13 @@ document.getElementById('lunch-menu5-3').innerText=''
 document.getElementById('china-special2-4').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='천원의 아침밥 - 돼지간장불고기 + 파인애플&드레싱 + 맑은순두부국'
+document.getElementById('lunch-menu1-4').innerText='학식모지 업데이트 일정으로 인해'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='순두부찌개'
+document.getElementById('lunch-menu2-4').innerText='화요일부터 서비스츨 재개할 예정입니다!'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='돼지간장불고기덮밥'
+document.getElementById('lunch-menu3-4').innerText='많은 기대 부탁드립니다!'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText='돈까스 + 크림스프 + 배추김치'
+document.getElementById('lunch-menu4-4').innerText=''
 //점심메뉴 5번 
 document.getElementById('lunch-menu5-4').innerText=''
 
@@ -69,13 +69,13 @@ document.getElementById('lunch-menu5-4').innerText=''
 document.getElementById('china-special2-5').innerText=''
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='천원의 아침밥 - 미트볼조림 + 펜네토마토소스무침 + 유부미소국'
+document.getElementById('lunch-menu1-5').innerText='학식모지 업데이트 일정으로 인해'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu2-5').innerText='화요일부터 서비스를 재개할 예정입니다!'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu3-5').innerText='많은 기대 부탁드립니다!'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText='메뉴 미정'
+document.getElementById('lunch-menu4-5').innerText=''
 //점심메뉴 5번 
 document.getElementById('lunch-menu5-5').innerText=''
 

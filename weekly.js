8c1beb8affb2e488df997c632ee76d7f18ad1129
ldH <92303070+dokdokorean@users.noreply.github.com>
2023-11-07 12:17:07 +0900

    Update weekly.js

--- weekly.js
+++ weekly.js
@@ -1,81 +1,81 @@
 //월요일
 
 //중식특식
-document.getElementById('china-special2-1').innerText=''
+document.getElementById('china-special2-1').innerText='마파두부덮밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-1').innerText='학식모지 업데이트 일정으로 인해'
+document.getElementById('lunch-menu1-1').innerText='육개장'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-1').innerText='화요일부터 서비스를 재개할 예정입니다!'
+document.getElementById('lunch-menu2-1').innerText='소고기무국'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-1').innerText='많은 기대 부탁드립니다!'
+document.getElementById('lunch-menu3-1').innerText='김치볶음밥&후라이'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-1').innerText=''
+document.getElementById('lunch-menu4-1').innerText='가츠동'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-1').innerText=''
+document.getElementById('lunch-menu5-1').innerText='일식카레&가라아게'
 
 //화요일
 
 //중식특식
-document.getElementById('china-special2-2').innerText=''
+document.getElementById('china-special2-2').innerText='고기짬뽕,고기짬뽕밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-2').innerText='학식모지 업데이트 일정으로 인해'
+document.getElementById('lunch-menu1-2').innerText='청국장찌개'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-2').innerText='화요일부터 서비스를 재개할 예정입니다!'
+document.getElementById('lunch-menu2-2').innerText='함박스테이크 set'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-2').innerText='많은 기대 부탁드립니다!'
+document.getElementById('lunch-menu3-2').innerText='야끼우동'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-2').innerText=''
+document.getElementById('lunch-menu4-2').innerText='사골우거지해장국'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-2').innerText=''
+document.getElementById('lunch-menu5-2').innerText='-'
 
 //수요일
 
 //중식특식
-document.getElementById('china-special2-3').innerText=''
+document.getElementById('china-special2-3').innerText='볶음밥&탕수육set'
 
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-3').innerText='학식모지 업데이트 일정으로 인해'
+document.getElementById('lunch-menu1-3').innerText='천원의 아침:유부주머니어묵탕,떡갈비,메추리알장조림'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-3').innerText='화요일부터 서비스를 재개할 예정입니다!'
+document.getElementById('lunch-menu2-3').innerText='소고기미역국'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-3').innerText='많은 기대 부탁드립니다!'
+document.getElementById('lunch-menu3-3').innerText='규동'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-3').innerText=''
+document.getElementById('lunch-menu4-3').innerText='나가사키짬뽕'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-3').innerText=''
+document.getElementById('lunch-menu5-3').innerText='반계탕'
 
 //목요일
 
 //중식특식
-document.getElementById('china-special2-4').innerText=''
+document.getElementById('china-special2-4').innerText='해물우동,해물덮밥'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-4').innerText='학식모지 업데이트 일정으로 인해'
+document.getElementById('lunch-menu1-4').innerText='천원의 아침:시래기된장국,계란야채볶음밥&케챱,비엔나조림,도시락김'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-4').innerText='화요일부터 서비스츨 재개할 예정입니다!'
+document.getElementById('lunch-menu2-4').innerText='부대찌개'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-4').innerText='많은 기대 부탁드립니다!'
+document.getElementById('lunch-menu3-4').innerText='스팸돈부리'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-4').innerText=''
+document.getElementById('lunch-menu4-4').innerText='참치마요덮밥'
 //점심메뉴 5번 
-document.getElementById('lunch-menu5-4').innerText=''
+document.getElementById('lunch-menu5-4').innerText='얼큰소고기콩나물해장국'
 
 //금요일
 
 //중식특식
-document.getElementById('china-special2-5').innerText=''
+document.getElementById('china-special2-5').innerText='-'
 
 //점심메뉴 1번 (국이름만 수정)
-document.getElementById('lunch-menu1-5').innerText='학식모지 업데이트 일정으로 인해'
+document.getElementById('lunch-menu1-5').innerText='천원의 아침:배추맑은국,제육,상추겉절이'
 //점심메뉴 2번 
-document.getElementById('lunch-menu2-5').innerText='화요일부터 서비스를 재개할 예정입니다!'
+document.getElementById('lunch-menu2-5').innerText='초밥&미니우동 set'
 //점심메뉴 3번 
-document.getElementById('lunch-menu3-5').innerText='많은 기대 부탁드립니다!'
+document.getElementById('lunch-menu3-5').innerText='-'
 //점심메뉴 4번 
-document.getElementById('lunch-menu4-5').innerText=''
+document.getElementById('lunch-menu4-5').innerText='-'
 //점심메뉴 5번 
 document.getElementById('lunch-menu5-5').innerText=''
 
